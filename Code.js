30ae29abb8daa81d7947c92ea7c3bc202d979ac9
tilda <iamtheworst@programmer.net>
2024-06-29 15:30:56 -0500

    separate oauth logic

--- Code.js
+++ Code.js
@@ -2,33 +2,6 @@ function convertToSeconds(ms) {
   return Math.floor(ms / 1000)
 }
 
-function getOauthToken() {
-  const props = PropertiesService.getUserProperties()
-  let secondsUntilExp = props.getProperty('OAUTH_TOKEN_EXPIRATION') - convertToSeconds(Date.now())
-
-  if (props.getProperty('OAUTH_TOKEN') !== null && ((props.getProperty('OAUTH_TOKEN_EXPIRATION') && secondsUntilExp >= 600))) {
-      Logger.log(`OAuth token valid for ${secondsUntilExp}s`)
-      return props.getProperty('OAUTH_TOKEN')
-  } else {
-      Logger.log('OAuth token is close to expiring/has expired, renewing')
-      var tokenRequest = UrlFetchApp.fetch('https://osu.ppy.sh/oauth/token', {
-        'headers': {
-          'Accept': 'application/json'
-        },
-        'method': 'post',
-        'payload': `client_id=${settings.CLIENT_ID}&client_secret=${settings.CLIENT_SECRET}&grant_type=client_credentials&scope=public`
-      })
-
-      var tokenResponse = JSON.parse(tokenRequest.getContentText())
-
-      props.setProperty('OAUTH_TOKEN', tokenResponse.access_token)
-      props.setProperty('OAUTH_TOKEN_EXPIRATION', (convertToSeconds(Date.now()) + tokenResponse.expires_in))
-      Logger.log('Renewed OAuth token')
-
-      return props.getProperty('OAUTH_TOKEN')
-  }
-}
-
 function _onEdit(e) {
   switch (e.range.getColumn()) {
     case 1: // Map ID update, populate info there
